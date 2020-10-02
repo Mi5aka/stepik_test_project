84f5b6ca80956aa80339ef349e2df687a31b822f
Mi5aka <mi5akatwi@gmail.com>
2020-10-02 12:19:03 +0200

    added admin panel with read-only models and filters

--- code_training/app/admin.py
+++ code_training/app/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Decision
+
+
+class DecisionAdmin(admin.ModelAdmin):
+    model = Decision
+    readonly_fields = []
+    list_filter = ('status', 'created_at')
+    list_display = ('id', 'status', 'created_at')
+
+    def get_readonly_fields(self, request, obj=None):
+        return list(self.readonly_fields) + \
+               [field.name for field in obj._meta.fields] + \
+               [field.name for field in obj._meta.many_to_many]
+
+    def has_add_permission(self, request):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+
+admin.site.register(Decision, DecisionAdmin)
